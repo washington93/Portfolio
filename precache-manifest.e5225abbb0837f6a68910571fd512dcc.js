ddf50885518def228356491b33f5cf21b5c2ae0f
Washington Silva <washington.s@escolar.ifrn.edu.br>
2020-08-08 19:24:00 -0300

    Updates

---
+++ precache-manifest.e5225abbb0837f6a68910571fd512dcc.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "c2c20a81ee2a788a7410231142e34872",
+    "url": "/index.html"
+  },
+  {
+    "revision": "56e683c81701a7268ff6",
+    "url": "/static/js/2.54a45228.chunk.js"
+  },
+  {
+    "revision": "3453b8997016469371284a28c0e873e2",
+    "url": "/static/js/2.54a45228.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "32a2740a97dea720e35b",
+    "url": "/static/js/main.12215884.chunk.js"
+  },
+  {
+    "revision": "d9ca30917d17d3d8f77e",
+    "url": "/static/js/runtime-main.f50b582c.js"
+  },
+  {
+    "revision": "db0503eb69f7f68555eed8a4b9e83433",
+    "url": "/static/media/foto_perfil.db0503eb.png"
+  }
+]);
